1c6b2174ad294a49f210ab7572cc0280971be036
Juan Pablo Tafur Corral <jtafur166@soyudemedellin.edu.co>
2024-08-09 16:32:32 -0500

    Se crea el metodo mover

--- Actividad_de_participacion_3/Punto.py
+++ Actividad_de_participacion_3/Punto.py
@@ -5,10 +5,25 @@ class Punto:
         self.y = y
 
     def mostrar(self):
-        print(f"Las Coordenadas del punto son {self.nombre}: ({self.x}, {self.y})")
+        print(f"Las Coordenadas del punto {self.nombre} son: ({self.x}, {self.y})")
+
+    def mover(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        print(f"El punto {self.nombre} se ha movido {dx} unidades en X y {dy} unidades en Y")
+
+    
+
 
 punto_a = Punto("A", 3, 4)
 punto_a.mostrar()  
 
 punto_b = Punto("B", -2, 1)
 punto_b.mostrar()  
+
+punto_a.mover(2, 1)  
+punto_a.mostrar() 
+
+punto_b.mover(-1, 3) 
+punto_b.mostrar()
+    
