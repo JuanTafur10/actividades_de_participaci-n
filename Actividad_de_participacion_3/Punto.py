3c63d9883c7fe8e84fd097a93840a939dc206590
Juan Pablo Tafur Corral <jtafur166@soyudemedellin.edu.co>
2024-08-09 16:43:55 -0500

    Se crea el metodo calcular_distancia

--- Actividad_de_participacion_3/Punto.py
+++ Actividad_de_participacion_3/Punto.py
@@ -12,18 +12,22 @@ class Punto:
         self.y += dy
         print(f"El punto {self.nombre} se ha movido {dx} unidades en X y {dy} unidades en Y")
 
+    def calcular_distancia(self, otro_punto):
+        dx = self.x - otro_punto.x
+        dy = self.y - otro_punto.y
+        distancia = (dx**2 + dy**2)**0.5
+        return print(f"La distancia entre el punto {self.nombre} y el punto {otro_punto.nombre} es: {distancia:.2f}")
+        
     
 
 
 punto_a = Punto("A", 3, 4)
+
 punto_a.mostrar()  
 
-punto_b = Punto("B", -2, 1)
-punto_b.mostrar()  
-
 punto_a.mover(2, 1)  
+
 punto_a.mostrar() 
 
-punto_b.mover(-1, 3) 
-punto_b.mostrar()
-    
+punto_c = Punto("C", 5, 0)
+punto_a.calcular_distancia(punto_c)
