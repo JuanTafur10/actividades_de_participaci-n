6de927e99c8372385a0a2ffa71dcc8b12711b1ca
Juan Pablo Tafur Corral <jtafur166@soyudemedellin.edu.co>
2024-08-09 16:25:51 -0500

    Se crea el metodo mostrar

--- Actividad_de_participacion_3/Punto.py
+++ Actividad_de_participacion_3/Punto.py
@@ -1,4 +1,14 @@
 class Punto:
-    def __init__(self, x, y):
+    def __init__(self, nombre,  x, y):
+        self.nombre = nombre        
         self.x = x
         self.y = y
+
+    def mostrar(self):
+        print(f"Las Coordenadas del punto son {self.nombre}: ({self.x}, {self.y})")
+
+punto_a = Punto("A", 3, 4)
+punto_a.mostrar()  
+
+punto_b = Punto("B", -2, 1)
+punto_b.mostrar()  
