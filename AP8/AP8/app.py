50f74be82773e9c8f4b2b824d97223dbf8aa2e20
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2024-10-09 02:27:11 -0500

    Se sgue trabajando en el resto del codigo

--- AP8/AP8/app.py
+++
@@ -1,5 +0,0 @@
-from tiendalibros.view.console import UIConsola
-
-if __name__ == "__main__":
-    consola = UIConsola()
-    consola.ejecutar_app()
--- AP8/tiendalibros/modelo/libro_existente_error.py
+++
@@ -1,9 +0,0 @@
-from tiendalibros.modelo.libro_error import LibroError
-
-
-class LibroExistenteError(LibroError):
-
-    pass
-    # Defina metodo inicializador
-
-    # Defina metodo especial
