6d50733336ccd6924a4f1e49c9d435928b0700e5
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2024-10-09 03:21:31 -0500

    Se implementa todo el codigo y se modifican algunas cosas

--- Actividad_de_participacion_8/tiendalibros/view/console.py
+++ Actividad_de_participacion_8/tiendalibros/view/console.py
@@ -1,10 +1,7 @@
 import sys
-
-from tiendalibros.modelo.tienda_libros import TiendaLibros
-
+from tiendalibros.modelo.tienda_libros import TiendaLibros, LibroExistenteError, LibroAgotadoError, ExistenciasInsuficientesError
 
 class UIConsola:
-
     def __init__(self):
         self.tienda_libros: TiendaLibros = TiendaLibros()
         self.opciones = {
@@ -38,8 +35,32 @@ class UIConsola:
             else:
                 print(f"{opcion} no es una opción válida")
 
-    # Defina el metodo retirar_libro_de_carrito_de_compras
+    def retirar_libro_de_carrito_de_compras(self):
+        isbn = input("Ingrese el ISBN del libro que desea retirar del carrito: ")
+        self.tienda_libros.retirar_item_de_carrito(isbn)
+        print(f"El libro con ISBN {isbn} ha sido retirado del carrito exitosamente.")
 
-    # Defina el metodo agregar_libro_a_carrito_de_compras
+    def agregar_libro_a_carrito_de_compras(self):
+        isbn = input("Ingrese el ISBN del libro que desea agregar al carrito: ")
+        cantidad = int(input("Ingrese la cantidad de unidades que desea agregar: "))
+        try:
+            libro = self.tienda_libros.catalogo[isbn]
+            self.tienda_libros.agregar_libro_a_carrito(libro, cantidad)
+            print(f"El libro con ISBN {isbn} ha sido agregado al carrito exitosamente.")
+        except KeyError:
+            print(f"El libro con ISBN {isbn} no se encuentra en el catálogo.")
+        except LibroAgotadoError as e:
+            print(e)
+        except ExistenciasInsuficientesError as e:
+            print(e)
 
-    # Defina el metodo adicionar_un_libro_a_catalogo
+    def adicionar_un_libro_a_catalogo(self):
+        isbn = input("Ingrese el ISBN del libro: ")
+        titulo = input("Ingrese el título del libro: ")
+        precio = float(input("Ingrese el precio del libro: "))
+        existencias = int(input("Ingrese las existencias del libro: "))
+        try:
+            self.tienda_libros.adicionar_libro_a_catalogo(isbn, titulo, precio, existencias)
+            print(f"El libro '{titulo}' con ISBN {isbn} ha sido añadido al catálogo exitosamente.")
+        except LibroExistenteError as e:
+            print(e)
