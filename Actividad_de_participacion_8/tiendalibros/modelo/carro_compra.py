a47a0b4353c5e87a6cd75fbd78a770ead32a5e2d
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2024-10-09 02:39:11 -0500

    Se implementa la clase CarroCompras y sus metodos

--- Actividad_de_participacion_8/tiendalibros/modelo/carro_compra.py
+++ Actividad_de_participacion_8/tiendalibros/modelo/carro_compra.py
@@ -1,9 +1,31 @@
+from item_compra import ItemCompra
+from libro import Libro
+
 class CarroCompras:
-    pass
-    # Defina metodo inicializador __init__(self)
+    def __init__(self):
+        self.items = []
 
-    # Defina el metodo agregar_item
+    def agregar_item(self, libro: Libro, cantidad: int) -> ItemCompra:
+        item = ItemCompra(libro, cantidad)
+        self.items.append(item)
+        return item
 
-    # Defina el metodo calcular_total
+    def calcular_total(self) -> float:
+        total = sum(item.calcular_subtotal() for item in self.items)
+        return total
 
-    # Defina el metodo quitar_item
+    def quitar_item(self, isbn: str):
+        self.items = [item for item in self.items if item.ibro.isbn != isbn]
+
+
+libro1 = Libro(titulo="El Quijote", autor="Miguel de Cervantes", precio=20.0, isbn="1234567890")
+libro2 = Libro(titulo="1984", autor="George Orwell", precio=15.0, isbn="0987654321")
+
+carro = CarroCompras()
+carro.agregar_item(libro1, 3)
+carro.agregar_item(libro2, 2)
+
+print(carro.calcular_total())  # Output: 90.0
+
+carro.quitar_item("1234567890")
+print(carro.calcular_total())  # Output: 30.0
