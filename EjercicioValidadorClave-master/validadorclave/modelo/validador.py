1239bf352e088b759558d10065edcc5a5fa1af87
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2024-10-29 22:31:25 -0500

    Se implementa el metodo _validar_longitud

--- EjercicioValidadorClave-master/validadorclave/modelo/validador.py
+++ EjercicioValidadorClave-master/validadorclave/modelo/validador.py
@@ -1,5 +1,12 @@
 from abc import ABC, abstractmethod
 
 class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
+
     @abstractmethod
     def es_valida(self, valor):
+        pass
+
+    def _validar_longitud(self, valor):
+        return len(valor) >= self._longitud_esperada
